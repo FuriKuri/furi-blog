c7398ed87a2886182b98ef7dc9457e2d91572c26
Theo Pack <tf.pack@googlemail.com>
2013-11-04 15:54:05 +0100

    move layout functions to layout.clj

--- src/furiblog/routes/home.clj
+++ src/furiblog/routes/home.clj
@@ -3,31 +3,10 @@
             [furiblog.views.layout :as layout]
             [furiblog.models.mongo :as mongo]))
 
-(defn format-time [timestamp]
-  (-> "dd/MM/yyyy"
-      (java.text.SimpleDateFormat.)
-      (.format timestamp)))
-
-(defn format-tags [tags]
-  (reduce
-   #(str %1 ", " %2)
-   (map
-    #(str "\"" %1 "\"")
-    tags)))
-
-(defn show-posts []
-  [:div.content
-   (for [{:keys [title content date tags]} (mongo/read-posts)]
-     [:div
-      [:hr]
-      [:h2 title]
-      [:p.post-content content]
-      [:p.post-footer "{ date: \"" (format-time date) "\", tags: [" (format-tags tags) "] }"]])])
-
 (defn home []
   (layout/common
    [:h1 "{ blog: \"FuriKuri\" }"]
-   (show-posts)))
+   (layout/blog-posts (mongo/read-posts))))
 
 (defroutes home-routes
   (GET "/" [] (home)))
--- src/furiblog/views/layout.clj
+++ src/furiblog/views/layout.clj
@@ -7,3 +7,24 @@
      [:title "Welcome to FuriKuri's Blog"]
      (include-css "/css/screen.css")]
     [:body body]))
+
+(defn format-time [timestamp]
+  (-> "dd/MM/yyyy"
+      (java.text.SimpleDateFormat.)
+      (.format timestamp)))
+
+(defn format-tags [tags]
+  (reduce
+   #(str %1 ", " %2)
+   (map
+    #(str "\"" %1 "\"")
+    tags)))
+
+(defn blog-posts [posts]
+  [:div.content
+   (for [{:keys [title content date tags]} posts]
+     [:div
+      [:hr]
+      [:h2 title]
+      [:p.post-content content]
+      [:p.post-footer "{ date: \"" (format-time date) "\", tags: [" (format-tags tags) "] }"]])])
